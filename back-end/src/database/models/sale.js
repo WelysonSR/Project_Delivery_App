ace8fa09efaabb27b0d4ffe7114f90736fbe5b55
Welyson Rosa <welysonsr@gmail.com>
2023-01-20 19:32:02 -0300

    feat: sequelize models

---
+++ back-end/src/database/models/sale.js
@@ -0,0 +1,48 @@
+const sale = (sequelize, DataTypes) => {
+  const sale = sequelize.define('sale', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      sellerId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      totalPrice: DataTypes.DECIMAL(9, 2),
+      deliveryAddress: DataTypes.STRING(100),
+      deliveryNumber: DataTypes.STRING(50),
+      saleDate: DataTypes.DATE,
+      status: DataTypes.STRING(50),
+    }, 
+    { 
+      timestamps: false,
+      underscored: true,
+    },
+  );
+
+  sale.associate = (models) => {
+    sale.belongsTo(models.user, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+
+    sale.belongsTo(models.user, {
+      foreignKey: 'sellerId',
+      as: 'seller',
+    });
+
+    sale.hasMany(models.salesProduct, {
+      foreignKey: 'saleId',
+      as: 'salesProducts',
+    });
+  };
+
+  return sale;
+};
+
+module.exports = sale;
