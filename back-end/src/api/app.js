89ead8c05b8733e91264151b58dd68bbd684b7b0
Lucas Moura <93lucasribeiro@gmail.com>
2023-01-23 19:30:26 -0300

    corrigindo erro de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
 const erros = require('../middleware/Error');
 const loginRoutes = require('../router/user');
-const cors = require("cors")
+
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
