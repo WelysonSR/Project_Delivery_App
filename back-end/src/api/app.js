234fe88fa7076ff375315131a50dfcada62777a7
Welyson <welysonsr@gmail.com>
2023-01-23 16:55:34 -0300

    feat: Estrutura de errors desenvolvida

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+require('express-async-errors');
+const erros = require('../middleware/Error');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(erros);
 
 module.exports = app;
