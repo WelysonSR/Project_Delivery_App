0dde571a0e8137f068949e9b9bb42ea3fc6bf796
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-01-25 16:36:17 -0400

    feat: adicionar o forms da página checkout

---
+++ front-end/src/pages/Checkout.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Navbar from '../components/NavBar';
+
+export default function Checkout() {
+  return (
+    <main>
+      <Navbar />
+      <form>
+        <p data-testid="customer_checkout__element-order-total-price">
+          Total:
+        </p>
+
+        <h4> Detalhes e Endereço para Entrega </h4>
+
+        <label htmlFor="vendedora">
+          P. Vendedora Responsável:
+          <select
+            id="vendedora"
+            data-testid="customer_checkout__select-seller"
+          >
+            <option> Fulana Pereira </option>
+          </select>
+        </label>
+
+        <label htmlFor="endereço">
+          Endereço:
+          <input
+            type="text"
+            id="endereço"
+            placeholder="Travessa Terceira da Castanheira, Bairro Muruci"
+            data-testid="customer_checkout__input-address"
+          />
+        </label>
+
+        <label htmlFor="address">
+          Número:
+          <input
+            type="text"
+            id="address"
+            placeholder="198"
+            data-testid="customer_checkout__input-address-number"
+          />
+        </label>
+
+        <button
+          type="button"
+          data-testid="customer_checkout__button-submit-order"
+        >
+          FINALIZAR PEDIDO
+        </button>
+      </form>
+    </main>
+  );
+}
--- front-end/src/routes/Routes.js
+++ front-end/src/routes/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import Checkout from '../pages/Checkout';
 
 function Routes() {
   return (
@@ -21,6 +22,10 @@ function Routes() {
         <Route
           path="/register"
           component={ Register }
+        />
+        <Route
+          path="/customer/checkout"
+          component={ Checkout }
         />
       </Switch>
     </div>
