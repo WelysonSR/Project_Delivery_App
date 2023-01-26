2df40ebd1f5219ffd78ab58b38ebdd83390ae25b
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-01-26 14:16:58 -0400

    feat: adiciona uma tabela generica

--- front-end/src/components/OrderTable.js
+++ front-end/src/components/OrderTable.js
@@ -1,49 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const tableNumber = 'customer_checkout__element-order-table-item-number-';
+const tableName = 'customer_checkout__element-order-table-name-';
+const tableQuantity = 'customer_checkout__element-order-table-quantity-';
+const tablePrice = 'customer_checkout__element-order-table-unit-price-';
+const tableTotal = 'customer_checkout__element-order-table-sub-total-';
+const tableRmv = 'customer_checkout__element-order-table-remove->';
 
 export default function OrderTable() {
+  const { generic, setGeneric } = useState();
+
+  const columns = () => (
+    <tr>
+      <th>Item</th>
+      <th>Descrição</th>
+      <th>Quantidade</th>
+      <th>Valor Unitário</th>
+      <th>Sub-total</th>
+    </tr>
+  );
+
+  const rmvItem = (item) => {
+    const remover = generic
+      .filter((e) => e.order !== Number(item));
+    setGeneric(remover);
+  };
+
+  const rows = () => {
+    if (generic.lenght !== undefined || generic.lenght !== 0) {
+      return (generic.map((e, i) => (
+        <tr key={ i }>
+          <td data-testid={ `${tableNumber}${i}` }>{ i + 1 }</td>
+          <td data-testid={ `${tableName}${i}` }>{e.name}</td>
+          <td data-testid={ `${tableQuantity}${i}` }>{e.quantity}</td>
+          <td data-testid={ `${tablePrice}${i}` }>{e.price}</td>
+          <td data-testid={ `${tableTotal}${i}` }>{e.price * e.quantity}</td>
+          <td>
+            <button
+              type="button"
+              id="deleteBtn"
+              data-testid={ `${tableRmv}${i}` }
+              onClick={ rmvItem }
+            >
+              Remover
+            </button>
+          </td>
+        </tr>
+      ))
+      );
+    }
+  };
+
   return (
     <main>
       <h4> Finalizar Pedido </h4>
       <table>
         <thead>
-          <tr>
-            <th
-              data-testid="customer_checkout__element-order-table-item-number-<index"
-            >
-              Item
-            </th>
-            <th
-              data-testid="customer_checkout__element-order-table-name-<index>"
-            >
-              Descrição
-            </th>
-            <th
-              data-testid="customer_checkout__element-order-table-quantity-<index>"
-            >
-              Quantidade
-            </th>
-            <th
-              data-testid="customer_checkout__element-order-table-unit-price-<index>"
-            >
-              Valor Unitário
-            </th>
-            <th
-              data-testid="customer_checkout__element-order-table-sub-total-<index>"
-            >
-              Sub-total
-            </th>
-          </tr>
+          { columns() }
         </thead>
         <tbody>
-          <tr>
-            <button
-              type="button"
-              id="deleteBtn"
-              data-testid="customer_checkout__element-order-table-remove-<index></index>"
-            >
-              Remover
-            </button>
-          </tr>
+          { rows() }
         </tbody>
       </table>
     </main>
