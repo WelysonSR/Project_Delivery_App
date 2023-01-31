e1be268b94af5b2adcc2ca4f4e3d3036a8c92306
Lucas Moura <93lucasribeiro@gmail.com>
2023-01-31 16:39:56 -0300

    reqs 30 a 32

--- front-end/src/components/SellerOrdersCard.js
+++ front-end/src/components/SellerOrdersCard.js
@@ -1,135 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
-
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import moment from 'moment/moment';
 
-export default function SellerOrdersCard({ details }) {
+export default function SellerOrdersCard({
+  id,
+  status,
+  saleDate,
+  totalPrice,
+  deliveryAddress,
+  deliveryNumber,
+}) {
   const [date, setDate] = useState('');
-  const [user, setUser] = useState({});
-  const [status, setStatus] = useState('Pendente');
-
-  const {
-    id: orderId,
-    // userId,
-    // sellerId,
-    totalPrice,
-    // deliveryAddress,
-    // deliveryNumber,
-    saleDate,
-    status: orderStatus,
-    // user,
-    products,
-  } = details;
 
   useEffect(() => {
     const momentDate = moment(saleDate);
     setDate(momentDate.format('DD/MM/YYYY'));
-    setUser(JSON.parse(localStorage.getItem('user')));
-    setStatus(orderStatus);
   }, []);
 
-  const changeStatus = async (param) => {
-    const URL = `http://localhost:3001/sales/${orderId}`;
-    await axios.patch(URL, { status: param }, {
-      headers: {
-        Authorization: user.token,
-      },
-    });
-    setStatus(param);
-  };
-
   return (
-    <div>
-      <div
-        data-testid="seller_order_details__element-order-details-label-order-id"
-      >
-        <p>{ `Pedido ${orderId}` }</p>
+    <Link to={ `/seller/orders/${id}` }>
+      <div data-testid={ `seller_orders__element-order-id-${id}` }>
+        <p>Pedido</p>
+        { id }
       </div>
-      <h4 data-testid="seller_order_details__element-order-details-label-order-date">
-        { date }
-      </h4>
-      <h2 data-testid="seller_order_details__element-order-details-label-delivery-status">
+      <h2 data-testid={ `seller_orders__element-delivery-status-${id}` }>
         { status }
       </h2>
-      <button
-        data-testid="seller_order_details__element-order-details-label-delivery-status"
-        onClick={ () => changeStatus('Preparando') }
-        type="button"
-      >
-        PREPARAR PEDIDO
-      </button>
-      <button
-        data-testid="seller_order_details__button-dispatch-check"
-        onClick={ () => changeStatus('Em Trânsito') }
-        type="button"
-      >
-        SAIU PARA ENTREGA
-      </button>
-      <table>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Descrição</th>
-            <th>Quantidade</th>
-            <th>Valor Unitário</th>
-            <th>Sub-total</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          { products.map(({ id, name, salesProduct, price }, index) => (
-            <tr key={ index }>
-              <td
-                data-testid={
-                  `seller_order_details__element-order-table-item-number-${index}`
-                }
-              >
-                { id }
-              </td>
-              <td
-                data-testid={
-                  `seller_order_details__element-order-table-name-${index}`
-                }
-              >
-                { name }
-              </td>
-              <td
-                data-testid={
-                  `seller_order_details__element-order-table-quantity-${index}`
-                }
-              >
-                { salesProduct.quantity }
-              </td>
-              <td
-                data-testid={
-                  `seller_order_details__element-order-table-unit-price-${index}`
-                }
-              >
-                <span>R$ </span>
-                { price.toString().replace('.', ',') }
-              </td>
-              <td
-                data-testid={
-                  `seller_order_details__element-order-table-sub-total-${index}`
-                }
-              >
-                <span>R$ </span>
-                { (Number(price) * Number(salesProduct.quantity)).toFixed(2)
-                  .toString().replace('.', ',') }
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <span>TOTAL: R$ </span>
-      <span
-        data-testid="seller_order_details__element-order-total-price"
-      >
-        { totalPrice.toString().replace('.', ',') }
-      </span>
-    </div>
+      <h4 data-testid={ `seller_orders__element-order-date-${id}` }>
+        { date }
+      </h4>
+      <h4 data-testid={ `seller_orders__element-card-price-${id}` }>
+        { `R$ ${totalPrice.replace('.', ',')}`}
+      </h4>
+      <h4 data-testid={ `seller_orders__element-card-address-${id}` }>
+        { `${deliveryAddress}, ${deliveryNumber}` }
+      </h4>
+    </Link>
   );
 }
 
--- front-end/src/pages/SellerOrdersDetails.js
+++ front-end/src/pages/SellerOrdersDetails.js
@@ -1,36 +1,162 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import moment from 'moment/moment';
 import NavBar from '../components/NavBar';
 import SellerOrdersCard from '../components/SellerOrdersCard';
 // import OrderDetailTable from '../components/OrderDetailsTable';
+// import { Link } from 'react-router-dom';
 
 export default function SellerOrderDetails() {
-  const [details, setDetails] = useState();
+  const [details, setDetails] = useState(null);
+  const [date, setDate] = useState('');
+  const [user, setUser] = useState({});
+  const [status, setStatus] = useState('Pendente');
+
   const { id: paramsId } = useParams();
 
   useEffect(() => {
     const getDetails = async () => {
       const { data } = await axios.get(`http://localhost:3001/sales/${paramsId}`);
-      console.log(data);
       setDetails(data);
+      const momentDate = moment(data.saleDate);
+      setDate(momentDate.format('DD/MM/YYYY'));
+      setUser(JSON.parse(localStorage.getItem('user')));
+      setStatus(data.status);
     };
     getDetails();
-  }, [paramsId]);
+  }, []);
+  // return (<div> oi</div>);
 
-  if (!details) {
-    return (
-      <>
-        <NavBar />
-        <p>Loading...</p>
-      </>
-    );
-  }
+  const changeStatus = async (param) => {
+    const URL = `http://localhost:3001/sales/${paramsId}`;
+    await axios.patch(URL, { status: param }, {
+      headers: {
+        Authorization: user.token,
+      },
+    });
+    setStatus(param);
+  };
 
   return (
-    <section>
+    <div>
       <NavBar />
-      <SellerOrdersCard details={ details } />
-    </section>
+      {!details && <p>Loading...</p>}
+      { details
+    && (
+      <>
+        <div
+          data-testid={
+            `seller_order_details__element-order-details-label-order-id-${paramsId}`
+          }
+        >
+          <p>{ `Pedido ${paramsId}` }</p>
+        </div>
+        <h4
+          data-testid={
+            `seller_order_details__element-order-details-label-order-date-${paramsId}`
+          }
+        >
+          { date }
+        </h4>
+        <h2
+          data-testid={ `
+      seller_order_details__element-order-details-label-delivery-status-${paramsId}
+      ` }
+        >
+          { status }
+        </h2>
+        <button
+          data-testid={ `
+        seller_order_details__element-order-details-label-delivery-status-${paramsId}
+        ` }
+          onClick={ () => changeStatus('Preparando') }
+          type="button"
+        >
+          PREPARAR PEDIDO
+        </button>
+        <button
+          data-testid={ `seller_order_details__button-dispatch-check-${paramsId}` }
+          onClick={ () => changeStatus('Em Trânsito') }
+          type="button"
+        >
+          SAIU PARA ENTREGA
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Valor Unitário</th>
+              <th>Sub-total</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            { details && details
+              .products.map(({ id, name, salesProduct, price }, index) => (
+                <tr key={ index }>
+                  <td
+                    data-testid={
+                      `seller_order_details__element-order-table-item-number-${index}`
+                    }
+                  >
+                    { id }
+                  </td>
+                  <td
+                    data-testid={
+                      `seller_order_details__element-order-table-name-${index}`
+                    }
+                  >
+                    { name }
+                  </td>
+                  <td
+                    data-testid={
+                      `seller_order_details__element-order-table-quantity-${index}`
+                    }
+                  >
+                    { salesProduct.quantity }
+                  </td>
+                  <td
+                    data-testid={
+                      `seller_order_details__element-order-table-unit-price-${index}`
+                    }
+                  >
+                    <span>R$ </span>
+                    { price.toString().replace('.', ',') }
+                  </td>
+                  <td
+                    data-testid={
+                      `seller_order_details__element-order-table-sub-total-${index}`
+                    }
+                  >
+                    <span>R$ </span>
+                    { (Number(price) * Number(salesProduct.quantity)).toFixed(2)
+                      .toString().replace('.', ',') }
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <span>TOTAL: R$ </span>
+        <span
+          data-testid="seller_order_details__element-order-total-price"
+        >
+          { details.totalPrice.toString().replace('.', ',') }
+        </span>
+      </>
+    )}
+    </div>
   );
 }
+
+SellerOrdersCard.propTypes = {
+  id: PropTypes.number,
+  status: PropTypes.string,
+  saleDate: PropTypes.string,
+  totalPrice: PropTypes.number,
+  deliveryAddress: PropTypes.string,
+  deliveryNumber: PropTypes.string,
+}.isRequired;
